d13e17555c8438ebff95fc1e8a85ce1ef593ce24
dtysky <dtysky@outlook.com>
2015-04-14 22:45:50 +0800

    Update.
    Signed-off-by: dtysky <dtysky@outlook.com>

--- ProjectBuilt/Gobang/Gobang.srcs/sources_1/bd/Gobang/hdl/Gobang_wrapper.v
+++ ProjectBuilt/Gobang/Gobang.srcs/sources_1/bd/Gobang/hdl/Gobang_wrapper.v
@@ -1,15 +1,15 @@
 //Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2014.4 (win64) Build 1071353 Tue Nov 18 18:29:27 MST 2014
-//Date        : Fri Apr 10 16:55:52 2015
+//Date        : Tue Apr 14 21:55:20 2015
 //Host        : Dtysky running 64-bit major release  (build 9200)
-//Command     : generate_target BoardPreGrid_wrapper.bd
-//Design      : BoardPreGrid_wrapper
+//Command     : generate_target Gobang_wrapper.bd
+//Design      : Gobang_wrapper
 //Purpose     : IP block netlist
 //--------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
 
-module BoardPreGrid_wrapper
+module Gobang_wrapper
    (I2C_SCLK,
     I2C_SDAT,
     clk_in1,
@@ -17,7 +17,8 @@ module BoardPreGrid_wrapper
     href,
     pclk,
     rst_n,
-    sel,
+    sel_act,
+    sel_view,
     vga_blue,
     vga_green,
     vga_hsync,
@@ -32,7 +33,8 @@ module BoardPreGrid_wrapper
   input href;
   input pclk;
   input rst_n;
-  input sel;
+  input sel_act;
+  input [1:0]sel_view;
   output [4:0]vga_blue;
   output [5:0]vga_green;
   output vga_hsync;
@@ -48,7 +50,8 @@ module BoardPreGrid_wrapper
   wire href;
   wire pclk;
   wire rst_n;
-  wire sel;
+  wire sel_act;
+  wire [1:0]sel_view;
   wire [4:0]vga_blue;
   wire [5:0]vga_green;
   wire vga_hsync;
@@ -57,7 +60,7 @@ module BoardPreGrid_wrapper
   wire vsync;
   wire xclk;
 
-BoardPreGrid BoardPreGrid_i
+Gobang Gobang_i
        (.I2C_SCLK(I2C_SCLK),
         .I2C_SDAT(I2C_SDAT),
         .clk_in1(clk_in1),
@@ -65,7 +68,8 @@ BoardPreGrid BoardPreGrid_i
         .href(href),
         .pclk(pclk),
         .rst_n(rst_n),
-        .sel(sel),
+        .sel_act(sel_act),
+        .sel_view(sel_view),
         .vga_blue(vga_blue),
         .vga_green(vga_green),
         .vga_hsync(vga_hsync),
